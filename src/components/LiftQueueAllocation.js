2615fdd29b55403e5816c298e76828c68a2fcb3f
Clare Forrest <clare.a.forrest@gmail.com>
2021-08-26 11:33:56 +1000

    create LiftQueueAllocation class

---
+++ src/components/LiftQueueAllocation.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+class LiftQueueAllocation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      requestDirection: 'up'
+    }
+  }
+};
+
+export default LiftQueueAllocation;
