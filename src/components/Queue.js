1545984e2bc44cdfc3017122dd92a749bc2f4194
Clare Forrest <clare.a.forrest@gmail.com>
2021-09-09 17:11:18 +1000

    add queue switching and lift location functions to Queue functional component

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,29 +1,65 @@
 import { useState } from 'react';
 
 const Queue = (props) => {
-  const queueType = props.queueType
+  // const queueType = props.queueType
+  const [queueType, setQueueType] = useState('up')
   const [upQueue, setUpQueue] = useState([])
   const [downQueue, setDownQueue] = useState([])
+  // Not sure I need liftMovingDirection at all 
+  // const [liftMovingDirection, setLiftMovingDirection] = useState(null)
+  const [liftLocation, setLiftLocation] = useState('G')
+  
+  const updateLiftLocation = (liftLocation, queueType, upQueue, downQueue ) => {
+    if(queueType === 'up' && upQueue.length > 0){
+      setLiftLocation(`${liftLocation} +1`)
+      return(
+        <h1>Lift Location: {liftLocation}</h1>
+        )
+    } else {
+      setLiftLocation(`${liftLocation} -1`)
+      return(
+        <h1>Lift Location: {liftLocation}</h1>
+        )
+    }
+  }
+  
+  setTimeout(updateLiftLocation, 1000, liftLocation, queueType, upQueue, downQueue)
 
   const {
     newCallLevel,
-    currentLiftLevel
   } = props;
 
-  if(queueType === 'down' && newCallLevel < currentLiftLevel){
+  if((queueType === 'down') && (newCallLevel < liftLocation)){
     // Place call at beginning of array
-    setQueue = upQueue(newCallLevel, ...upQueue)
+    setUpQueue(upQueue(newCallLevel, ...upQueue))
+    console.log('upQueue', upQueue)
+    setTimeout()
     return(
       upQueue
-      )
-    }else{
+    )
+  } else {
     // Place call at end of array
-    setQueue = downQueue(...downQueue, newCallLevel)
+    setDownQueue(downQueue(...downQueue, newCallLevel))
+    setTimeout()
     return(
       downQueue
     )
   }
 }
+
+function queueSwitching(queueType, upQueue, downQueue){
+  // const [liftMovingDirection, setLiftMovingDirection] = useState('up')
+  if((queueType === 'up' && upQueue.length === 0) && (queueType === 'down' && downQueue.length === 0)) {
+    console.log('reset to ground')
+  } else if (queueType === 'up' && upQueue.length === 0){
+    console.log('switch to down queue')
+  } else if (queueType === 'down' && downQueue.length === 0){
+    console.log('switch to up queue')
+  } else {
+
+  }
+}
+
 export default Queue;
 
 
