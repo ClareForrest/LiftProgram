1368de4705b0fdbc2c9b47a2c24f3009147caa8c
Clare Forrest <clare.a.forrest@gmail.com>
2021-09-09 11:33:11 +1000

    update function to include additional form for destinationRequest, update onClick attributes, change how state is set

--- src/components/LiftStatusState.js
+++ src/components/LiftStatusState.js
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
+import liftLocation from './LiftLocation';
 
 // Lift state is passed default values, then set with new user input
 const LiftStatusState = () => {
-  const initialState = {
-    liftCalled: false,
-    liftLocation: 0,
-    callLocation: null,
-    userDirectionRequest: null,
-    userDestinationRequest: null,
-    liftMovingDirection: null,
+
+  const [liftCalled, setLiftCalled] = useState(false)
+  const [callLocation, setCallLocation] = useState(null)
+  const [userDirectionRequest, setUserDirectionRequest] = useState(null)
+  const [userDestinationRequest, setUserDestinationRequest] = useState(null)
+  const [liftMovingDirection, setLiftMovingDirection] = useState(null)
+
+
+  function onClickCallLocation(event) {
+    // event.preventDefault()
+    setCallLocation = event.target.value
+    setLiftCalled = true
+    // setLiftLocation = //LiftLocation functional Component 
+    
   }
 
-  const
+  function onDestinationRequest(event){
+    setUserDestinationRequest = event.target.value
+  }
 
-
-  function onCallLocationClick(event) {
-    event.preventDefault()
-    setCallLocation = event.target.value
+  // The below, OR liftDirection = queue (up or down)
+  function liftDirection(liftLocation, callLocation){
+    if (liftLocation < callLocation){
+      setLiftMovingDirection = 'up'
+      return(liftMovingDirection)
+    } else {
+      setLiftMovingDirection = 'down'
+      return(liftMovingDirection)
+    }
   }
 
   function onDirectionClick(event) {
@@ -32,10 +47,10 @@ const LiftStatusState = () => {
     } else {
       setLiftMovingDirection('down')
     }
-    console.log("onDirectionClick return", onDirectionClick)
+    console.log("onDirectionClick return")
     return(
       <>
-        {/* {liftCalled} */}
+        {liftCalled}
       </>
     )
   }
@@ -43,23 +58,32 @@ const LiftStatusState = () => {
   <>
     <form>
       <label>Select which floor you are on</label>
-      <button type='submit' value='Ground' onClick={(e) => onCallLocationClick}>G</button>
-      <button type='submit' value='One' onClick={(e) => setCallLocation(e.target.value)}>1</button>
-      <button type='submit' value='Two' onClick={(e) => setCallLocation(e.target.value)}>2</button>
-      <button type='submit' value='Three' onClick={(e) => setCallLocation(e.target.value)}>3</button>
-      <button type='submit' value='Four' onClick={(e) => setCallLocation(e.target.value)}>4</button>
-      <button type='submit' value='Five' onClick={(e) => setCallLocation(e.target.value)}>5</button>
+      <button type='submit' value='Ground' onClick={onClickCallLocation}>G</button>
+      <button type='submit' value='One' onClick={onClickCallLocation}>1</button>
+      <button type='submit' value='Two' onClick={onClickCallLocation}>2</button>
+      <button type='submit' value='Three' onClick={onClickCallLocation}>3</button>
+      <button type='submit' value='Four' onClick={onClickCallLocation}>4</button>
+      <button type='submit' value='Five' onClick={onClickCallLocation}>5</button>
     </form>
 
     <form>
-      <button type='submit' value={userDirectionRequest} onClick={(e) => onDirectionClick(e)}>UP</button>
-      <button type='submit' value={userDirectionRequest} onClick={(e) => onDirectionClick(e)}>DOWN</button>
+      <label>Up or Down?</label>
+      <button type='submit' value={userDirectionRequest} onClick={onDirectionClick} >UP</button>
+      <button type='submit' value={userDirectionRequest} onClick={onDirectionClick}  >DOWN</button>
     </form>
-    <h1>Call Location: {callLocation}</h1>
-    <h1>Lift Location: {liftLocation}</h1>
-    <h1>User Direction Request: {userDirectionRequest}</h1>
-    <h1>Lift Moving Direction: {liftMovingDirection}</h1>
 
+    <form>
+    <label>Choose your destination</label>
+      <button type='submit' value={userDestinationRequest} >UP</button>
+      <button type='submit' value={userDestinationRequest} >DOWN</button>
+    </form>
+
+      <h1>Lift Called: {liftCalled}</h1>
+      <h1>Lift Location: {liftLocation}</h1>
+      <h1>Call Location: {callLocation}</h1>
+      <h1>User Direction Request: {userDirectionRequest}</h1>
+      <h1>User Destination Request: {userDestinationRequest}</h1>
+      <h1>Lift Moving Direction: {liftMovingDirection}</h1>
   </>
   )
 }
