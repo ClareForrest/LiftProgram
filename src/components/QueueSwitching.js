311da73fb07e45c9d2a4e132277b714a7abd3399
Clare Forrest <clare.a.forrest@gmail.com>
2021-09-09 11:31:54 +1000

    convert Class function to arrow, add in switch case

--- src/components/QueueSwitching.js
+++ src/components/QueueSwitching.js
@@ -1,13 +1,17 @@
-import React, { useState, useEfect } from 'react';
+import DownQueue from './DownQueue';
+import UpQueue from './UpQueue';
 
-class QueueSwitching extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      upQueueEmpty: true,
-      downQueueEmpty: true,
-    }
+function QueueSwitching(props){
+  switch(props.direction, props.length){
+    case ((props.direction = 'up' && props.length === 0) && (props.direction === 'down' && props.length === 0)):
+      console.log('reset to ground')
+      break;
+    case (props.direction === 'up' && props.length === 0):
+      DownQueue()
+      break;
+    case (props.direction === 'down' && props.length === 0):
+      UpQueue()
+      break;
   }
-};
-
+}
 export default QueueSwitching;
