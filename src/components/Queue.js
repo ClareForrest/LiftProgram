cf59b0d09114097916f00dc20ba86080cd692adc
Clare Forrest <clare.a.forrest@gmail.com>
2021-09-10 15:37:41 +1000

    combine queueType & liftUpdate functions, store liftQueueAllocation func comp

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,65 +1,82 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const Queue = (props) => {
-  // const queueType = props.queueType
-  const [queueType, setQueueType] = useState('up')
+  const [queueType, setQueueType] = useState(props.queueType)
   const [upQueue, setUpQueue] = useState([])
   const [downQueue, setDownQueue] = useState([])
-  // Not sure I need liftMovingDirection at all 
-  // const [liftMovingDirection, setLiftMovingDirection] = useState(null)
-  const [liftLocation, setLiftLocation] = useState('G')
+  const [liftLocation, setLiftLocation] = useState(0)
+  const {
+    callLocation,
+    userDestinationRequest
+  } = props;
   
-  const updateLiftLocation = (liftLocation, queueType, upQueue, downQueue ) => {
-    if(queueType === 'up' && upQueue.length > 0){
-      setLiftLocation(`${liftLocation} +1`)
-      return(
-        <h1>Lift Location: {liftLocation}</h1>
-        )
-    } else {
+  const updateLiftLocationAndQueue = (liftLocation, queueType, upQueue, downQueue ) => {
+    // Make this into a loop? 
+    if((upQueue.length === 0) && (downQueue.length === 0)) {
+      console.log('reset to ground')
+    } else if (queueType === 'up'){
+      if (upQueue.length > 0){
+        setLiftLocation(`${liftLocation} +1`)
+        return(
+          <>
+            <h1>Lift Location: {liftLocation}</h1>
+            <h1>Queue Type: {queueType}</h1>
+          </>
+          )
+      } else if (upQueue.length === 0){
+        setQueueType('down')
+        console.log("switching to down queue")
+      }
+    } else if (queueType === 'down')
+      if (downQueue.length > 0) {
       setLiftLocation(`${liftLocation} -1`)
       return(
-        <h1>Lift Location: {liftLocation}</h1>
+        <>
+          <h1>Lift Location: {liftLocation}</h1>
+          <h1>Queue Type: {queueType}</h1>
+        </>
         )
+      } else if (downQueue.length === 0){
+        setQueueType('up')
+        console.log("switching to up queue")
+      }
+  }
+  
+  setTimeout(updateLiftLocationAndQueue, 1000, liftLocation, queueType, upQueue, downQueue)
+
+  const liftQueueAllocation = (queueType, liftLocation) => {
+    if(callLocation && (queueType === 'up')){
+      if(callLocation < liftLocation) {
+        // Place call at beginning of array
+        setUpQueue(upQueue(callLocation, ...upQueue))
+      } else if (callLocation > liftLocation){
+        // Place call at end of array
+        setUpQueue(upQueue(...upQueue, callLocation))
+      }
+    } if (userDestinationRequest && (queueType === 'up')){
+      if(userDestinationRequest < liftLocation) {
+        // Place call at beginning of array
+        setUpQueue(upQueue(userDestinationRequest, ...upQueue))
+      } else if (userDestinationRequest > liftLocation){
+        // Place call at end of array
+        setUpQueue(upQueue(...upQueue, userDestinationRequest))
+      }
+    } if(callLocation && (queueType === 'down')){
+      if(callLocation < liftLocation) {
+        // Place call at beginning of array
+        setDownQueue(downQueue(callLocation, ...downQueue))
+      } else if (callLocation > liftLocation){
+        // Place call at end of array
+        setDownQueue(downQueue(...downQueue, callLocation))
+      }
+    } if (userDestinationRequest && (queueType === 'down')){
+      setDownQueue(downQueue(callLocation, ...downQueue))
+    } else if (callLocation > liftLocation){
+      // Place call at end of array
+      setDownQueue(downQueue(...downQueue, callLocation))
     }
   }
   
-  setTimeout(updateLiftLocation, 1000, liftLocation, queueType, upQueue, downQueue)
-
-  const {
-    newCallLevel,
-  } = props;
-
-  if((queueType === 'down') && (newCallLevel < liftLocation)){
-    // Place call at beginning of array
-    setUpQueue(upQueue(newCallLevel, ...upQueue))
-    console.log('upQueue', upQueue)
-    setTimeout()
-    return(
-      upQueue
-    )
-  } else {
-    // Place call at end of array
-    setDownQueue(downQueue(...downQueue, newCallLevel))
-    setTimeout()
-    return(
-      downQueue
-    )
-  }
-}
-
-function queueSwitching(queueType, upQueue, downQueue){
-  // const [liftMovingDirection, setLiftMovingDirection] = useState('up')
-  if((queueType === 'up' && upQueue.length === 0) && (queueType === 'down' && downQueue.length === 0)) {
-    console.log('reset to ground')
-  } else if (queueType === 'up' && upQueue.length === 0){
-    console.log('switch to down queue')
-  } else if (queueType === 'down' && downQueue.length === 0){
-    console.log('switch to up queue')
-  } else {
-
-  }
 }
 
 export default Queue;
-
-
