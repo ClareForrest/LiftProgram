9c5b5189bc3cf2a75fa4aad934cba0c7a140b64a
Clare Forrest <clare.a.forrest@gmail.com>
2021-09-19 09:53:52 +1000

    convert if statement to switch case

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { render } from 'react-dom';
 
 const Queue = (props) => {
   const [queueType, setQueueType] = useState(props.queueType)
@@ -11,38 +12,70 @@ const Queue = (props) => {
   } = props;
   
   const updateLiftLocationAndQueue = (liftLocation, queueType, upQueue, downQueue ) => {
-    // Make this into a loop? 
-    if((upQueue.length === 0) && (downQueue.length === 0)) {
-      console.log('reset to ground')
-    } else if (queueType === 'up'){
-      if (upQueue.length > 0){
-        setLiftLocation(`${liftLocation} +1`)
-        return(
-          <>
-            <h1>Lift Location: {liftLocation}</h1>
-            <h1>Queue Type: {queueType}</h1>
-          </>
-          )
-      } else if (upQueue.length === 0){
-        setQueueType('down')
-        console.log("switching to down queue")
-      }
-    } else if (queueType === 'down')
-      if (downQueue.length > 0) {
-      setLiftLocation(`${liftLocation} -1`)
-      return(
-        <>
-          <h1>Lift Location: {liftLocation}</h1>
-          <h1>Queue Type: {queueType}</h1>
-        </>
-        )
-      } else if (downQueue.length === 0){
-        setQueueType('up')
-        console.log("switching to up queue")
-      }
+    // Make this into a loop? or switch case? 
+
+    switch (queueType) {
+      case 'up':
+        if (upQueue.length > 0){
+          setLiftLocation(`${liftLocation +1}`)
+          return liftLocation
+        } else if (upQueue.length === 0){
+          console.log("switching to down queue")
+          setQueueType('down')
+          return queueType
+        }
+        break;
+      case 'down':
+        if (downQueue.length > 0) {
+          setLiftLocation(`${liftLocation -1}`)
+          return liftLocation
+          } else if (downQueue.length === 0){
+            console.log("switching to up queue")
+            setQueueType('up')
+            return queueType
+          }
+        break;
+      default: 
+        console.log('reset to ground')
+        break;
+    }
+    return(
+      <>
+        <h1>Lift Location: {liftLocation}</h1>
+        <h1>Queue Type: {queueType}</h1>
+      </>
+      )
+    // if((upQueue.length === 0) && (downQueue.length === 0)) {
+    //   console.log('reset to ground')
+    // } else if (queueType === 'up'){
+    //   if (upQueue.length > 0){
+    //     setLiftLocation(`${liftLocation} +1`)
+    //     return(
+    //       <>
+    //         <h1>Lift Location: {liftLocation}</h1>
+    //         <h1>Queue Type: {queueType}</h1>
+    //       </>
+    //       )
+    //   } else if (upQueue.length === 0){
+    //     setQueueType('down')
+    //     console.log("switching to down queue")
+    //   }
+    // } else if (queueType === 'down')
+    //   if (downQueue.length > 0) {
+    //   setLiftLocation(`${liftLocation} -1`)
+    //   return(
+    //     <>
+    //       <h1>Lift Location: {liftLocation}</h1>
+    //       <h1>Queue Type: {queueType}</h1>
+    //     </>
+    //     )
+    //   } else if (downQueue.length === 0){
+    //     setQueueType('up')
+    //     console.log("switching to up queue")
+    //   }
   }
   
-  setTimeout(updateLiftLocationAndQueue, 1000, liftLocation, queueType, upQueue, downQueue)
+  // setTimeout(updateLiftLocationAndQueue, 1000, liftLocation, queueType, upQueue, downQueue)
 
   const liftQueueAllocation = (queueType, liftLocation) => {
     if(callLocation && (queueType === 'up')){
@@ -76,6 +109,7 @@ const Queue = (props) => {
       setDownQueue(downQueue(...downQueue, callLocation))
     }
   }
+
   return( 
   <>
     <h1>in the queue</h1>
