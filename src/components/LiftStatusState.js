33e3ff6e79ee74f7928a72f8b185bed5160d6525
Clare Forrest <clare.a.forrest@gmail.com>
2021-10-17 17:44:48 +1100

    extract display of lift levels and direction into functional component

--- src/components/LiftStatusState.js
+++ src/components/LiftStatusState.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
 import FloorDisplay from './FloorDisplay';
 import Queue from './Queue';
 
@@ -9,24 +10,25 @@ const LiftStatusState = () => {
 	const [callLocation, setCallLocation] = useState(null);
 	const [userDirectionRequest, setUserDirectionRequest] = useState(null);
 	const [userDestinationRequest, setUserDestinationRequest] = useState(null);
-	const [liftLocation, setLiftLocation] = useState(0)
+	const [liftLocation, setLiftLocation] = useState(0);
 	const [upQueue, setUpQueue] = useState([0])
-  const [downQueue, setDownQueue] = useState([])
-	// const [upQueue, setUpQueue] = useState([1,3,4,6,7,8])
-  // const [downQueue, setDownQueue] = useState([9,7,4,2,1])
-	
+	const [downQueue, setDownQueue] = useState([0])
+	// const [upQueue, setUpQueue] = useState([1, 3, 4, 6, 7, 8]);
+	// const [downQueue, setDownQueue] = useState([9, 7, 4, 2, 1]);
 
 	useEffect(() => {
-		return <>{(userDirectionRequest, userDestinationRequest, upQueue, downQueue)}</>;
-	}, []);
-
+		return (
+			<>{(userDirectionRequest, userDestinationRequest, upQueue, downQueue)}</>
+		);
+	});
+s
 	function onUpDirectionClick(event) {
 		event.preventDefault();
 
-		setCallLocation(getRandomCallLocation(0, 10));
 		setLiftCalled(true);
+		setCallLocation(getRandomCallLocation(0, 10));
 		setUserDirectionRequest('up');
-		liftQueueAllocation(userDirectionRequest, callLocation)
+		liftQueueAllocation(userDirectionRequest, callLocation);
 	}
 
 	function onDownDirectionClick(event) {
@@ -35,14 +37,14 @@ const LiftStatusState = () => {
 		setCallLocation(getRandomCallLocation(0, 10));
 		setLiftCalled(true);
 		setUserDirectionRequest('down');
-		liftQueueAllocation(userDirectionRequest, callLocation)
+		liftQueueAllocation(userDirectionRequest, callLocation);
 	}
 
 	function onDestinationRequest(event) {
 		event.preventDefault();
 		setUserDestinationRequest(event.target.value);
 		// Need to know what queue is currently in action (up or down) getQueue function??
-		destinationLiftQueueAllocation(userDestinationRequest, queueType)
+		destinationLiftQueueAllocation(userDestinationRequest, queueType);
 	}
 
 	// A random selector for floor level instead of user having to select (better imitates real life).
@@ -52,134 +54,166 @@ const LiftStatusState = () => {
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
 
-	  // This only addresses lift calls from each level (not within the lift itself)
-		// TO DO: check callLocation against items in array. Don't add if it already exists
-		// TO DO: move to another file and import 
-		function liftQueueAllocation(userDirectionRequest, callLocation){
-			if(userDirectionRequest === 'up'){
-				if(callLocation < upQueue[0]) {
-					let newQueue = [callLocation, ...upQueue]
-					setUpQueue(newQueue)
-					console.log('upQueue', upQueue)
-				} else if (callLocation > upQueue[0]){
-					let newQueue = [...upQueue, callLocation]
-					setUpQueue(newQueue)
-
-					console.log('upQueue', upQueue)
-				}
-			} else if(userDirectionRequest === 'down'){
-				if(callLocation < downQueue[0]){
-					let newQueue = [...downQueue, callLocation]
-					setDownQueue(newQueue)
-					console.log('down queue', downQueue)
-				} else if(callLocation > downQueue[0]){
-					let newQueue = [callLocation, ...downQueue]
-					setDownQueue(newQueue)
-					console.log('down queue', downQueue)
-				}
+	// This only addresses lift calls from each level (not within the lift itself)
+	// TO DO: check callLocation against items in array. Don't add if it already exists
+	function liftQueueAllocation(userDirectionRequest, callLocation) {
+		if (userDirectionRequest === 'up') {
+			if (callLocation < upQueue[0]) {
+				let newQueue = [callLocation, ...upQueue];
+				setUpQueue(newQueue);
+				console.log('userDirectionRequest - upQueue', upQueue);
+			} else if (callLocation > upQueue[0]) {
+				let newQueue = [...upQueue, callLocation];
+				setUpQueue(newQueue);
+				console.log('userDirectionRequest - upQueue', upQueue);
 			}
-			floorDisplay(queueType, upQueue, downQueue)
-		}
-
-		// TO DO: move to another file and import 
-		function destinationLiftQueueAllocation(userDestinationRequest, queueType){
-			if(queueType === 'up'){
-				if(userDestinationRequest <= upQueue[0]){
-					let newQueue = [Number(userDestinationRequest), ...upQueue]
-					setUpQueue(newQueue)
-					console.log('up queue', upQueue)
-				} else if(userDestinationRequest >= upQueue[0]){
-					let newQueue = [...upQueue, Number(userDestinationRequest)]
-					setUpQueue(newQueue)
-					console.log('up queue', upQueue)
-				}
-			} else if (queueType === 'down'){
-				if(userDestinationRequest <= downQueue[0]){
-					let newQueue = [...downQueue, Number(userDestinationRequest)]
-					setDownQueue(newQueue)
-				} else if(userDestinationRequest >= downQueue[0]){
-					let newQueue = [Number(userDestinationRequest), ...downQueue]
-					setDownQueue(newQueue)
-					console.log('down queue', downQueue)
-				}
+		} else if (userDirectionRequest === 'down') {
+			if (callLocation < downQueue[0]) {
+				let newQueue = [...downQueue, callLocation];
+				setDownQueue(newQueue);
+				console.log('userDirectionRequest - down queue', downQueue);
+			} else if (callLocation > downQueue[0]) {
+				let newQueue = [callLocation, ...downQueue];
+				setDownQueue(newQueue);
+				console.log('userDirectionRequest - down queue', downQueue);
 			}
 		}
+	}
 
-		function floorDisplay(queueType, upQueue, downQueue ){
-			setTimeout(() => {
-				if(upQueue.length === 0 || downQueue.length === 0){
-					let newType = upQueue ? 'down' : 'up'
-					setQueueType(newType)
-					console.log('newqueue type', queueType)
-				}
-				if(queueType === 'up' && upQueue.length > 0){
-					setLiftLocation(liftLocation +1)
-					if(liftLocation === upQueue[0]){
-						upQueue.shift()
-						console.log('up queue', upQueue)
-					}
-				} else if (queueType === 'down' && downQueue.length > 1){
-					setLiftLocation(liftLocation -1)
-					if(liftLocation === downQueue[0]){
-						downQueue.shift()
-						console.log('down queue', downQueue)
-					}
-				}
-			}, 1000)
-		
-			return(
-				<>
-				<h1>FloorDisplay Component</h1>
-				<h3>Lift Level: {liftLocation}</h3>
-				</>
-			)
+	function destinationLiftQueueAllocation(userDestinationRequest, queueType) {
+		if (queueType === 'up') {
+			if (userDestinationRequest <= upQueue[0]) {
+				let newQueue = [Number(userDestinationRequest), ...upQueue];
+				setUpQueue(newQueue);
+				console.log('userDestinationRequest - up queue', upQueue);
+			} else if (userDestinationRequest >= upQueue[0]) {
+				let newQueue = [...upQueue, Number(userDestinationRequest)];
+				setUpQueue(newQueue);
+				console.log('userDestinationRequest - up queue', upQueue);
+			}
+		} else if (queueType === 'down') {
+			if (userDestinationRequest <= downQueue[0]) {
+				let newQueue = [...downQueue, Number(userDestinationRequest)];
+				setDownQueue(newQueue);
+				console.log('userDestinationRequest - down queue', downQueue);
+			} else if (userDestinationRequest >= downQueue[0]) {
+				let newQueue = [Number(userDestinationRequest), ...downQueue];
+				setDownQueue(newQueue);
+				console.log('userDestinationRequest - down queue', downQueue);
+			}
 		}
+	}
 
+	const floorDisplay = (queueType, upQueue, downQueue) => {
+		setTimeout(() => {
+			if (queueType === 'up' && upQueue.length > 0) {
+				setLiftLocation(liftLocation + 1);
+				console.log('liftLocation', liftLocation);
+				if (liftLocation === upQueue[0]) {
+					upQueue.shift();
+					console.log('up queue', upQueue);
+				}
+				if(upQueue.length === 0){
+					setQueueType('down')
+				}
+			} else if (queueType === 'down' && downQueue.length > 1) {
+				setLiftLocation(liftLocation - 1);
+				console.log('liftLocation', liftLocation);
+				if (liftLocation === downQueue[0]) {
+					downQueue.shift();
+					console.log('down queue', downQueue);
+				}
+				if(downQueue.length === 0){
+					setQueueType('up')
+				}
+			}
+		}, 1000);
+	}
+
+	// while(upQueue.length > 0){
+	// 	floorDisplay(queueType, upQueue, downQueue)
+	// }
+
+
+	// TO DO: check callLocation against items in array. Don't add if it already exists
+	function liftQueueAllocation(userDirectionRequest, callLocation) {
+		if (userDirectionRequest === 'up') {
+			if (callLocation < upQueue[0]) {
+				let newQueue = [callLocation, ...upQueue];
+				setUpQueue(newQueue);
+				console.log('upQueue', upQueue);
+			} else if (callLocation > upQueue[0]) {
+				let newQueue = [...upQueue, callLocation];
+				setUpQueue(newQueue);
+				console.log('upQueue', upQueue);
+			}
+		} else if (userDirectionRequest === 'down') {
+			if (callLocation < downQueue[0]) {
+				let newQueue = [...downQueue, callLocation];
+				setDownQueue(newQueue);
+				console.log('down queue', downQueue);
+			} else if (callLocation > downQueue[0]) {
+				let newQueue = [callLocation, ...downQueue];
+				setDownQueue(newQueue);
+				console.log('down queue', downQueue);
+			}
+		}
+	}
+
+	function destinationLiftQueueAllocation(userDestinationRequest, queueType) {
+		if (queueType === 'up') {
+			if (userDestinationRequest < upQueue[0]) {
+				let newQueue = [callLocation, ...upQueue];
+				setUpQueue(newQueue);
+			} else if (userDestinationRequest > upQueue[0]) {
+				let newQueue = [...upQueue, callLocation];
+				setUpQueue(newQueue);
+			}
+		} else if (queueType === 'down') {
+			if (userDestinationRequest < downQueue[0]) {
+				let newQueue = [...downQueue, callLocation];
+				setDownQueue(newQueue);
+			} else if (userDestinationRequest > downQueue[0]) {
+				let newQueue = [callLocation, ...downQueue];
+				setDownQueue(newQueue);
+			}
+		}
+	}
+
+	const LiftButtonDisplay = (props) => {
+		return <button onClick={onDestinationRequest}> {props.level} </button>
+	}
+
+	const UpDownButton = (props) => {
+		return <button onClick={onDownDirectionClick}> {props.direction} </button>
+	}
+
+	const liftLevels = [1,2,3,4,5,6]
 	return (
 		<>
-			<form>
-				{/* Change this to appear as two buttons that display as arrows (up and down) */}
-				{/* TO DO: convert to inputs with radio selection */}
-				<label>User Select: </label>
-				<button type='submit' onClick={onUpDirectionClick}>
-					UP
-				</button>
-				<button type='submit' onClick={onDownDirectionClick}>
-					DOWN
-				</button>
-			</form>
+			{/* Change this to appear as two buttons that display as arrows (up and down) */}
+			{/* TO DO: convert to inputs with radio selection */}
+			<UpDownButton direction={'up'} />
+			<UpDownButton direction={'down'} />
 			<hr />
-			{/* Once this is a display button, trigger to be green once selected  */}
+			<div>
+			{/* TO DO: Make this visible only once lift reaches destination to mimick stepping into the lift */}
+				{liftLevels.map(level => {
+					<LiftButtonDisplay key={level} level={level}/>
+					console.log('level', level)
+				})}
+			</div>
 			<p>
 				User Direction Request:
 				{userDirectionRequest
 					? userDirectionRequest
 					: 'User Direction Request unknown'}
 			</p>
-			{/* This is automatically generated to imitate real lift */}
 			<p>
 				Call Location:
 				{callLocation ? callLocation : 'Call location unknown'}
 			</p>
-
-			{/* Make this visible only once lift reaches destination to mimick stepping into the lift?? */}
-			{/* Change to input selector instead of buttons  */}
-
-			<div onChange={onDestinationRequest}>
-				<label>Choose your destination</label>
-				{/* TO DO: have values displayed from mapped options const  */}
-				<input type='radio' value={1} name='destinationRequest' /> 1
-				<input type='radio' value={2} name='destinationRequest' /> 2
-				<input type='radio' value={3} name='destinationRequest' /> 3
-				<input type='radio' value={4} name='destinationRequest' /> 4
-				<input type='radio' value={5} name='destinationRequest' /> 5
-				<input type='radio' value={6} name='destinationRequest' /> 6
-				<input type='radio' value={7} name='destinationRequest' /> 7
-				<input type='radio' value={8} name='destinationRequest' /> 8
-				<input type='radio' value={9} name='destinationRequest' /> 9
-				<input type='radio' value={10} name='destinationRequest' /> 10
-				<p> Destination Request: {userDestinationRequest} </p>
-			</div>
+			<p>Lift Location: {liftLocation} </p>
 		</>
 	);
 };
